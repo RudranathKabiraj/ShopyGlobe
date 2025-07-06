ea503a59edf583a4ab3831455199e85d38c95bb7
RudranathKabiraj <kabirajrudranath@gmail.com>
2025-07-06 20:33:11 +0530

    Initial ShoppyGlobe project with deploy setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 // src/main.jsx
 import React from "react";
-
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store"; // import the store
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/Shoppy_Globe">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
