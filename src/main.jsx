fc245a4bf6c756756f6ff97a51d3ec4c53c34959
RudranathKabiraj <kabirajrudranath@gmail.com>
2025-07-07 20:31:05 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Shoppy_Globe">
+      <BrowserRouter basename="/ShopyGlobe">
         <App />
       </BrowserRouter>
     </Provider>
